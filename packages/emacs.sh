9388c73b2efd464e09b1dd5d1203e3cd2ecc83b4
Chris Langhans <chris@langhans-coding.de>
2020-10-07 21:51:12 +0200

    emacs build script

--- packages/emacs.sh
+++ packages/emacs.sh
@@ -1,45 +1,23 @@
 #!/usr/bin/env bash
-
-set -x
-
-# check command result, print message and exit
-# $1: error code
-# $2: error message
-function checkResult {
-    if [ "${1}" -ne "0" ]; then
-        echo "Error[$1]: $2" >&2
-        exit 1
-    fi
-}
+set -euxo pipefail
 
 # cleanup on exit
-# function cleanup {
-
-#     if [ -d emacs ]; then
-#         rm -rf emacs
-#     fi
-# }
-# trap cleanup EXIT
+function cleanup {
+    if [ -d emacs ]; then
+        rm -rf emacs
+    fi
+}
+trap cleanup EXIT
 
 # download
 git clone --depth 1 git://git.savannah.gnu.org/emacs.git
-checkResult 0 "git clone failed. Abort."
 
 pushd emacs
 ./autogen.sh
 
-./configure --prefix=/home/$(whoami)/package/emacs --with-x --with-xml2
-checkResult $? "configure failed. Abort."
+./configure --prefix=/home/$1/package/emacs --with-x --with-xml2
 
 make -j8 all
-checkResult $? "make failed. Abort."
 
 make install
-checkResult $? "installation failed. Abort."
-popd
-
-pushd ~/package/
-sudo stow -v -S emacs/ -t /usr/local --defer=share/info/dir
-checkResult $? "stow failed. Abort."
-
 popd
