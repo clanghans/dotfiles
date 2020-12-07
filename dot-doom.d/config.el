e728c2537e91e382838a8c1b3f3869ed13bf5323
Chris Langhans <chris@langhans-coding.de>
2020-12-07 07:21:24 +0100

    keymaps updated, evil cursor magic, added ws-butler

--- dot-doom.d/config.el
+++ dot-doom.d/config.el
@@ -25,7 +25,12 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-acario-dark)
+;; (setq doom-theme 'doom-monokai)
+;; (setq doom-theme 'doom-dark+)
+;; (setq doom-theme 'doom-material)
+;; (setq doom-theme 'doom-nord)
+;; (setq doom-theme 'doom-palenight)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -57,15 +62,97 @@
 
 (map!
  (:leader
-  (:desc "M-x" "SPC" #'counsel-M-x)))
+  :desc "M-x" "SPC" #'counsel-M-x
+  )
+ (:leader
+  :prefix ("j" . "jump")
+  :desc "+default/search-buffer" "s" #'+default/search-buffer
+  :desc "avy-goto-char-timer" "j" #'avy-goto-char-timer
+  )
+ (:leader
+  :prefix "w"
+  :desc "delete-other-window" "1" #'delete-other
+  )
+ (:leader
+  :prefix "o"
+  :desc "org-brain-visualize" "v" #'org-brain-visualize
+  )
+ )
 
 (after! magit
   (setq magit-save-repository-buffers 'dontask
         magit-repository-directories '(("/home/raziel/Workspace/" . 3))
         )
   )
-;; org - Ar1a
-(setq org-bullets-bullet-list '("#"))
+
+(after! org
+  (setq org-bullets-bullet-list '("#"))
+  )
 
 ;; Interesting packages
 ;; carbon-now-sh
+
+(after! org-brain
+  (setq org-brain-path "~/org/brain")
+  )
+
+;; (setq-default cmake-tab-width 4)
+;; (setq-default c-basic-offset 4)
+(setq-hook! 'cmake-mode-hook
+  cmake-tab-width 4
+  +format-with-lsp nil
+  )
+
+;; disable code formatting in specific modes
+;; (setq +format-on-save-enabled-modes
+;;       '(not cmake-mode)
+;;       )
+;;
+
+;; always recenter on going off screen
+(setq scroll-conservatively 0)
+(setq scroll-margin 10)
+
+(setq evil-escape-unordered-key-sequence t)
+;; evil search commands cross line endings
+(setq evil-cross-lines t)
+;; don't move cursor when exiting insert mode
+(setq evil-move-cursor-back nil)
+
+;; whitespace wars
+(require 'ws-butler)
+(add-hook 'prog-mode-hook #'ws-butler-mode)
+
+(after! org-brain
+  (evil-set-initial-state 'org-brain-visualize-mode 'emacs)
+  )
+
+;; the next hero awakens
+(require 'hercules)
+
+(hercules-def
+ :toggle-funs #'org-brain-visualize-mode
+ :hide-funs '(org-brain-visualize-quit org-brain-open-resource org-brain-goto-child)
+ :keymap 'org-brain-visualize-mode-map
+ :transient t
+ )
+
+;;
+;; clangd
+;;
+(defun clanghans/compile-command-link ()
+  (interactive)
+  "Create a symbolic link from the root directory to the compile-command.json file."
+  (when (projectile-project-p)
+    (defvar clanghans/cc_json)
+    (let clanghans/cc_json "compile_commands.json")
+    ;; find file in project root - recursive
+    (defvar clanghans/target)
+    (let clanghans/target (directory-files-recursively (projectile-project-root) clanghans/cc_json))
+    ;; full path to relative
+    (defvar clanghans/relative-target)
+    (let clanghans/relative-target (file-relative-name (car clanghans/target) (projectile-project-root)))
+    ;; creating the symbolic link
+    (make-symbolic-link clanghans/relative-target (concat (projectile-project-root) clanghans/cc_json) t)
+    )
+  )
