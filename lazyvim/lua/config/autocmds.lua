0405e9aaf2009ab8a0cbc0afd06c978fef90d1c0
Chris Langhans <lcn2abt@de.bosch.com>
2024-12-18 09:25:52 +0100

    format changes

--- lazyvim/lua/config/autocmds.lua
+++ lazyvim/lua/config/autocmds.lua
@@ -2,15 +2,14 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-
 -- Now, create the autocommand within that group
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'markdown',
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
   callback = function()
     -- Assuming that you are using the built-in LSP, you can disable diagnostics like this:
     vim.diagnostic = require("vim.diagnostic")
     vim.diagnostic.enable(false)
-  end
+  end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
