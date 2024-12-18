8410a68186079ab552e410e9455b7ba5925cb2a9
Chris Langhans <lcn2abt@de.bosch.com>
2024-12-18 09:22:27 +0100

    changed obsidian which-key group

--- lazyvim/lua/plugins/obsidian.lua
+++ lazyvim/lua/plugins/obsidian.lua
@@ -1,46 +1,58 @@
 return {
-  "epwalsh/obsidian.nvim",
-  version = "*", -- use latest release instead of latest commit
-  lazy = false,
-  ft = "markdown",
-  dependencies = {
-    -- Required.
-    "nvim-lua/plenary.nvim",
-  },
-  opts = {
-    workspaces = {
-      {
-        name = "personal",
-        path = "~/vaults/personal",
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- use latest release instead of latest commit
+    lazy = false,
+    ft = "markdown",
+    dependencies = {
+      -- Required.
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "personal",
+          path = "~/vaults/personal",
+        },
+        {
+          name = "work",
+          path = "~/vaults/work",
+        },
       },
-      {
-        name = "work",
-        path = "~/vaults/work",
+    },
+    mappings = {
+      -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+      ["gf"] = {
+        action = function()
+          return require("obsidian").util.gf_passthrough()
+        end,
+        opts = { noremap = false, expr = true, buffer = true },
+      },
+      -- Toggle check-boxes.
+      ["<leader>ch"] = {
+        action = function()
+          return require("obsidian").util.toggle_checkbox()
+        end,
+        opts = { buffer = true },
       },
     },
-  },
-  mappings = {
-    -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-    ["gf"] = {
-      action = function()
-        return require("obsidian").util.gf_passthrough()
-      end,
-      opts = { noremap = false, expr = true, buffer = true },
-    },
-    -- Toggle check-boxes.
-    ["<leader>ch"] = {
-      action = function()
-        return require("obsidian").util.toggle_checkbox()
-      end,
-      opts = { buffer = true },
+    keys = {
+      { "<leader>on", "<cmd>ObsidianNew<cr>", desc = "Obsidian New" },
+      { "<leader>of", "<cmd>ObsidianQuickSwitch<cr>", desc = "Obsidian File Search" },
+      { "<leader>os", "<cmd>ObsidianSearch<cr>", desc = "Obsidian Search" },
+      { "<leader>or", "<cmd>ObsidianRename<cr>", desc = "Obsidian Rename" },
+      { "<leader>ot", "<cmd>ObsidianToday<cr>", desc = "Obsidian Today" },
+      { "<leader>oy", "<cmd>ObsidianYesterday<cr>", desc = "Obsidian Yesterday" },
     },
   },
-  keys = {
-    { "<leader>nn", "<cmd>ObsidianNew<cr>", desc = "Obsidian New" },
-    { "<leader>nf", "<cmd>ObsidianQuickSwitch<cr>", desc = "Obsidian File Search" },
-    { "<leader>ns", "<cmd>ObsidianSearch<cr>", desc = "Obsidian Search" },
-    { "<leader>nr", "<cmd>ObsidianRename<cr>", desc = "Obsidian Rename" },
-    { "<leader>nt", "<cmd>ObsidianToday<cr>", desc = "Obsidian Today" },
-    { "<leader>ny", "<cmd>ObsidianYesterday<cr>", desc = "Obsidian Yesterday" },
+  {
+    "folke/which-key.nvim",
+    opts = {
+      spec = {
+        {
+          { "<leader>o", group = "Obsidian" },
+        },
+      },
+    },
   },
 }
