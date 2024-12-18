d51adb7cdd63ee97037fd17a32a9c4196d653c26
Chris Langhans <lcn2abt@de.bosch.com>
2024-12-18 09:26:10 +0100

    remove telescope dependencies

--- lazyvim/lua/plugins/git.lua
+++ lazyvim/lua/plugins/git.lua
@@ -5,32 +5,12 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim", -- required
       "sindrets/diffview.nvim", -- optional - Diff integration
-      "nvim-telescope/telescope.nvim", -- optional
     },
     config = true,
+    lazy = false,
     keys = {
       { "<leader>gg", "<cmd>Neogit<cr>", "Neogit" },
       { "<leader>gG", "<cmd>Neogit cwd=getcwd()<cr>", "Neogit" },
-    },
-  },
-  {
-    "emmanueltouzery/agitator.nvim",
-    dependencies = { "telescope.nvim", "nvim-lua/plenary.nvim" },
-    keys = {
-      {
-        "<leader>gt",
-        function()
-          require("agitator").git_time_machine()
-        end,
-        desc = "git time machine",
-      },
-      {
-        "<leader>gb",
-        function()
-          require("agitator").git_blame()
-        end,
-        desc = "git blame",
-      },
     },
   },
 }
