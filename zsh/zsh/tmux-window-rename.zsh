5ea63c69ca1786b1ce3d83db8c542fcaf62891d6
Chris Langhans <lcn2abt@de.bosch.com>
2025-12-04 11:49:58 +0100

    remove tmux-window-rename plugin and use built-in functions

--- zsh/zsh/tmux-window-rename.zsh
+++
@@ -1,7 +0,0 @@
-function tmux-window-name() {
-	if [ -x "$TMUX_PLUGIN_MANAGER_PATH/tmux-window-name/scripts/rename_session_windows.py" ]; then
-		("$TMUX_PLUGIN_MANAGER_PATH/tmux-window-name/scripts/rename_session_windows.py" &)
-	fi
-}
-
-add-zsh-hook chpwd tmux-window-name
