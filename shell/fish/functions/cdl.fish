7f52de413f7ca9d99a163ab5f4280041edcc73a7
Chris Langhans <chris@langhans.dev>
2026-02-05 08:00:41 +0100

    delete unnecessary file

--- shell/fish/functions/cdl.fish
+++
@@ -1,7 +0,0 @@
-function cdl
-    echo "$history[1]" | read --array --tokenize result
-
-    # Expand the tilde (~) to the full home directory path
-    set -l last_arg (string replace "~" $HOME $result[-1])
-    cd (dirname (realpath $last_arg))
-end
