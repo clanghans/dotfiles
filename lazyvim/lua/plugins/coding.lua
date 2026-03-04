12514b5a2877f05a6b3f0d94a9692d01d7c83802
Chris Langhans <chris@langhans.dev>
2026-03-04 16:34:14 +0100

    lsp corrections

--- lazyvim/lua/plugins/coding.lua
+++ lazyvim/lua/plugins/coding.lua
@@ -5,9 +5,9 @@ return {
     optional = true,
     opts = {
       formatters_by_ft = {
-        python = { "ruff", "ruff_fix", "ruff_format" },
+        python = { "ruff_fix", "ruff_format" },
         shell = { "shfmt" },
-        zsh = { "shfmt" },
+        -- zsh = { "shfmt" }, -- shfmt doesn't support zsh syntax (e.g. $+commands[])
         -- dart = { "dart format" },
       },
     },
@@ -41,14 +41,6 @@ return {
             return { "--python-executable", python_executable }
           end,
         },
-        -- -- Example of using selene only when a selene.toml file is present
-        -- selene = {
-        --   -- `condition` is another LazyVim extension that allows you to
-        --   -- dynamically enable/disable linters based on the context.
-        --   condition = function(ctx)
-        --     return vim.fs.find({ "selene.toml" }, { path = ctx.filename, upward = true })[1]
-        --   end,
-        -- },
       },
     },
   },
