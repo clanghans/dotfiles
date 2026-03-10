16871a2f62d73c042bdb336db53d5d5393a9d329
Chris Langhans <chris@langhans.dev>
2026-03-10 15:38:49 +0100

    reworked install.sh, added ripgrep config, removed cd alias

--- install.sh
+++ install.sh
@@ -10,12 +10,10 @@ usage() {
   echo
   echo "Options:"
   echo "  --all              Install all dotfiles"
-  echo "  --shell            Install shell"
-  echo "  --zshrc            Install .zshrc"
-  echo "  --gitconfig        Install .gitconfig"
+  echo "  --shell            Install shell environment"
+  echo "  --tmux             Install tmux config"
   echo "  --i3               Install i3 config"
   echo "  --fonts            Install fonts"
-  echo "  --tmux             Install .tmux.conf"
   echo "  --alacritty        Install alacritty config"
   echo "  --ghostty          Install ghostty config"
   echo "  --nix              Install nix config"
@@ -70,29 +68,7 @@ check_prerequisits() {
   fi
 }
 
-# install_pkgx() {
-#   if ! command -v pkgx --version &>/dev/null; then
-#     curl -fsS https://pkgx.sh | sh
-#   fi
-#
-#   export PATH="$HOME/.local/bin:$PATH"
-# }
-
 install_nix() {
-  # pkgx install jq
-  # pkgx install fzf
-  # pkgx install ripgrep
-  # pkgx install bat
-  # pkgx install fd
-  # pkgx install eza
-  # pkgx install dust
-  # pkgx install htop
-  #
-  # pkgx install cmake
-  # pkgx install shellcheck
-  # pkgx install cargo
-  # pkgx install podman
-  #
 
   if ! command -v nix --version &>/dev/null; then
     curl -L https://nixos.org/nix/install | sh
@@ -106,66 +82,26 @@ install_nix() {
 install_tmux() {
   local tmux_conf_dir="${XDG_CONFIG_HOME}/tmux"
 
-  if ! command -v tmux -V &>/dev/null; then
-    pkgx install tmux
-  fi
-
   mkdir -p "${tmux_conf_dir}/plugins"
   create_symlink "tmux/tmux.conf" "${tmux_conf_dir}/tmux.conf"
   git_clone_or_update "https://github.com/tmux-plugins/tpm" "${tmux_conf_dir}/plugins/tpm"
 }
 
 install_shell() {
-  if ! command -v fish -v &>/dev/null; then
-    echo "fish shell not available -> apt install fish"
-
-  else
-    fish -c "
-if not functions -q fisher
-    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-end
-fisher install IlanCosman/tide@v6
-fisher install kidonng/zoxide.fish
-fisher install PatrickF1/fzf.fish
-
-tide configure --auto --style=Lean --prompt_colors='True color' --show_time=No --lean_prompt_height='Two lines' --prompt_connection=Disconnected --prompt_spacing=Compact --icons='Many icons' --transient=Yes
-"
-  fi
-
-  ./shell/xdg_setup
-
-  create_symlink "shell/fish/config.fish" "${XDG_CONFIG_HOME}/fish/config.fish"
-
-  for source_file in shell/fish/functions/*; do
-    target_file="${XDG_CONFIG_HOME}/fish/functions/$(basename "$source_file")"
-    create_symlink "$source_file" "$target_file"
-  done
-}
-
-install_zsh() {
-  # if ! command -v zsh --version &>/dev/null; then
-  # TODO install zsh
-  # pkgx install zsh
-  # fi
-
-  # install antigen
-  mkdir -p "${XDG_CONFIG_HOME}/antigen"
-  curl -L git.io/antigen >"${XDG_CONFIG_HOME}/antigen/antigen.zsh"
 
   create_symlink "zsh/zshrc" "${HOME}/.zshrc"
   create_symlink "zsh/zshenv" "${HOME}/.zshenv"
   create_symlink "zsh/zsh" "${HOME}/.zsh"
 
-  echo "run 'chsh -s /bin/zsh' to change default shell to zsh"
-}
-
-install_git() {
-  if ! command -v git --version &>/dev/null; then
-    pkgx install git
-  fi
+  mkdir -p "${XDG_CONFIG_HOME}/ripgrep"
+  create_symlink "ripgrep/config" "${XDG_CONFIG_HOME}/ripgrep/config"
 
   mkdir -p "${XDG_CONFIG_HOME}/git"
   create_symlink "git/config" "${XDG_CONFIG_HOME}/git/config"
+
+  create_symlink "lazyvim" "${XDG_CONFIG_HOME}/nvim"
+
+  echo "run 'chsh -s /bin/zsh' to change default shell to zsh"
 }
 
 install_i3() {
@@ -184,10 +120,6 @@ install_i3() {
   mkdir -p "${XDG_CONFIG_HOME}/polybar"
   create_symlink "i3/polybar.config" "${XDG_CONFIG_HOME}/polybar/config.ini"
   create_symlink "i3/polybar-launcher" "${LOCAL_BIN}/polybar-launcher"
-}
-
-install_neovim() {
-  create_symlink "lazyvim" "${XDG_CONFIG_HOME}/nvim"
 }
 
 install_fonts() {
@@ -255,23 +187,10 @@ main() {
       install_specific+=("shell")
       shift
       ;;
-    --zsh)
-      install_specific+=("zsh")
-      shift
-      ;;
-    --git)
-      install_specific+=("git")
-      shift
-      ;;
     --i3)
       install_specific+=("i3")
       shift
       ;;
-    --neovim)
-      install_specific+=("neovim")
-      shift
-      ;;
-
     --fonts)
       install_specific+=("fonts")
       shift
@@ -307,32 +226,26 @@ main() {
 
   # Install all dotfiles if --all is specified
   if [ "$install_all" = true ]; then
-    install_tmux
-    install_zsh
-    install_shell
-    install_git
-    install_i3
-    install_neovim
-    install_fonts
     install_alacritty
+    install_fonts
     install_ghostty
-    install_nix
     install_hyprland
+    install_i3
+    install_nix
+    install_shell
+    install_tmux
   else
     # Install only the specified dotfiles
     for section in "${install_specific[@]}"; do
       case "$section" in
-      tmux) install_tmux ;;
-      zsh) install_zsh ;;
-      shell) install_shell ;;
-      git) install_git ;;
-      i3) install_i3 ;;
-      neovim) install_neovim ;;
-      fonts) install_fonts ;;
       alacritty) install_alacritty ;;
+      fonts) install_fonts ;;
       ghostty) install_ghostty ;;
-      nix) install_nix ;;
       hyprland) install_hyprland ;;
+      i3) install_i3 ;;
+      nix) install_nix ;;
+      shell) install_shell ;;
+      tmux) install_tmux ;;
       esac
     done
   fi
